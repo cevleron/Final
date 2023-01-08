674457611a8682254363a328396e8ac3b1c180e7
Anton <Cevleron@yandex.ru>
2023-01-08 16:48:06 +0300

    update final

--- Final/Program.cs
+++ Final/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] array = new string[] { "hello", "2", "world", ":-)" };
+string[] array2 = new string[array.Length];
+
+void NewMassive(string[] array, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            array2[count] = array[i];
+            count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]}");
+    }
+    Console.Write("]");
+}
+NewMassive(array, array2);
+PrintArray(array2);
